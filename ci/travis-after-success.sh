e974ecdbd540f30665181dce9dfab8b30cbcacc3
Paul Ramsey <pramsey@cleverelephant.ca>
2020-01-24 10:02:04 -0800

    only push to docker if we are building in master

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -18,10 +18,12 @@ fi
 if [ "$TARGET" = "docker" ]; then
     VERSION=`./$PROJECT --version | cut -f2 -d' '`
     DATE=`date +%Y%m%d`
-    docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
     docker build -f Dockerfile.ci --build-arg VERSION=$VERSION -t $DOCKER_REPO:$TAG .
     docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:$DATE
-    docker push $DOCKER_REPO
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+        docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+        docker push $DOCKER_REPO
+    fi
 else
     mkdir upload
     zip -r upload/${PROJECT}_${TAG}_${TARGET}.zip ${BINARY} README.md LICENSE.md config.toml assets/
